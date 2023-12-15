c2cd6b0abd542a46d23a87a2a72711aa206a7aa8
Denis Wolter <wolter.important@gmail.com>
2023-12-15 11:49:39 +0300

    add navbar

--- src/features/Home/HomePage.js
+++ src/features/Home/HomePage.js
@@ -1,8 +1,10 @@
 import '../../normalize.css';
+import NavBar from "../../components/NavBar/NavBar";
 
 function HomePage() {
   return (
     <div>
+      <NavBar />
       <p>home page</p>
     </div>
   );
