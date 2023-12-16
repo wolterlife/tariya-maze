08ac64fab7328b1cf7af84265053757df620e903
Denis Wolter <wolter.important@gmail.com>
2023-12-16 12:35:12 +0300

    add line in navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,16 +4,19 @@ import {Link} from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div className='navbar'>
-      <div className='content'>
-        <Link to='/news' className='text'>Новости</Link>
-        <Link to='/menu' className='text'>Меню</Link>
-        <Link to='/' className='text'>
-          <img src='/img/logo.png' alt='logo'/>
-        </Link>
-        <Link to='/discounts' className='text'>Акции</Link>
-        <Link to='/profile' className='text'>Профиль</Link>
+    <div className='containerNav'>
+      <div className='navbar'>
+        <div className='content'>
+          <Link to='/news' className='text'>Новости</Link>
+          <Link to='/menu' className='text'>Меню</Link>
+          <Link to='/' className='text'>
+            <img src='/img/logo.png' alt='logo'/>
+          </Link>
+          <Link to='/discounts' className='text'>Акции</Link>
+          <Link to='/profile' className='text'>Профиль</Link>
+        </div>
       </div>
+      <hr color='#AC9168' className='line'/>
     </div>
   );
 };
