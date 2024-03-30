95a535d3c4197bdf927dbbafe5883a57684b5273
Denis Wolter <wolter.important@gmail.com>
2024-03-30 17:02:05 +0300

    add background img in PopUpProfile

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -5,7 +5,7 @@ const History = () => {
   return (
     <div className='historyContainer'>
       <p className='textLarge'>Наша история</p>
-      <img src="/img/history-lines.svg" alt="history"/>
+      <img className='imgWithLines' src="/img/history-lines.svg" alt="history"/>
     </div>
   );
 };
--- src/components/PopUpProfile/PopUpProfile.js
+++ src/components/PopUpProfile/PopUpProfile.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './PopUpProfile.scss'
 import {useDispatch} from "react-redux";
 import {setPopUpProfileVisible} from "../../redux/commonSlice";
+import {useNavigate} from 'react-router-dom';
 const PopUpProfile = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   return (
     <div className='popUpProfile'>
       <div className="content">
-          <p>Войдите в ваш аккаунт</p>
+          <p className='textEntry'>Войдите в ваш аккаунт</p>
           <input onClick={() => dispatch(setPopUpProfileVisible(false))} className='buttonClose' type='image' src="/img/sn-tg.svg" alt="X"/>
         <div className="line">
           <p className='text'>Email / № телефона</p>
@@ -18,6 +20,10 @@ const PopUpProfile = () => {
           <input className='input' type="password"/>
         </div>
         <button className="buttonLogin">Войти</button>
+        <p className='textInfo'>Если нет аккаунта необходимо пройти регистрацию</p>
+        <input onClick={() => navigate('/registration')} className='buttonRegister' type='button' value='Зарегистрироваться' />
+        <img className='backgroundTop' src="/img/reg-top.svg" alt="background-img"/>
+        <img className='backgroundBot' src="/img/reg-bot.svg" alt="background-img"/>
       </div>
     </div>
   );
