899c64dab37ce37a81544a9be8b0bdd036cf41e4
Denis Wolter <wolter.important@gmail.com>
2024-04-02 16:32:11 +0300

    add img in RegistrationSection.js

--- src/components/RegistrationSection/RegistrationSection.js
+++ src/components/RegistrationSection/RegistrationSection.js
@@ -5,10 +5,13 @@ import FormRegistration from '../FormRegistration/FormRegistration';
 const RegistrationSection = () => {
   return (
     <div className='registration'>
-      <FormRegistration />
-        <div>
-        <p className='textTop'>Добро</p>
-        <p className='textBot'>пожаловать!!!</p>
+      <FormRegistration/>
+      <div>
+        <div className="containerText">
+          <p className='textTop'>Добро</p>
+          <p className='textBot'>пожаловать!!!</p>
+        </div>
+        <img className='image' src="/img/reg-img.svg" alt="image"/>
       </div>
     </div>
   );
