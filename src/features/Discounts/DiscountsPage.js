9141ca4f31a6871a500d41849652f3bdadec74e2
Denis Wolter <wolter.important@gmail.com>
2024-04-03 15:33:57 +0300

    add discounts page

--- src/features/Discounts/DiscountsPage.js
+++ src/features/Discounts/DiscountsPage.js
@@ -1,10 +1,15 @@
 import '../../normalize.css';
+import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
+import Swiper from "../../components/Swiper/Swiper";
 
 function DiscountsPage() {
   return (
-    <div>
-      <p>discounts page</p>
-    </div>
+    <>
+      <NavBar />
+      <Swiper />
+      <Footer />
+    </>
   );
 }
 
